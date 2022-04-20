f62787aca2893759f8d2ba28bd71e915b1d552f0
Isaí Velasco Hernández <velascohernandes.2017@gmail.com>
2022-04-20 00:04:54 -0500

    Show RSS in View - map with map error

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { Component, useState, useEffect } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native"
 import { useNavigation } from "@react-navigation/native"
 import { AppRegistry } from 'react-native';
@@ -6,27 +6,44 @@ import * as rssParser from 'react-native-rss-parser';
 var rssv;
 var title = "No title";
 var items = "No items";
+
 const HomeScreen = () => {
-    const navigation = useNavigation();
+
+    const [allowances, setAllowances] = useState([]);
+    useEffect(() => {
+        fetch('http://www.nasa.gov/rss/dyn/breaking_news.rss')
+            .then((response) => response.text())
+            .then((responseData) => rssParser.parse(responseData))
+            .then((rss) => {
+
+                setAllowances(rss);
+                console.log(rss.items.length);
+                rss.items.forEach(item => {
+                    console.log(item.title);
+                });
+
+            });
+    }, []);
+    /* 
     const getNews = () => {
         return fetch('http://www.nasa.gov/rss/dyn/breaking_news.rss')
             .then((response) => response.text())
             .then((responseData) => rssParser.parse(responseData))
             .then((rss) => {
-                rssv = rss;
-                console.log(rss.title);
+
 
                 console.log(rss.items.length);
                 rss.items.forEach(item => {
                     console.log(item.title);
                 });
-            });
-    }
 
-    getNews();
-    
+            });
+    }*/
+    const navigation = useNavigation();
+
     return (
         <View>
+
             <Text
                 style={{
                     fontSize: 30,
@@ -35,17 +52,18 @@ const HomeScreen = () => {
                 }}
             > Home Screen
             </Text>
-            <Text style={{
+            <Text id="title" style={{
                 textAlign: "center"
             }}>
-                {title}
+                {allowances.title}
             </Text>
             <TouchableOpacity
                 onPress={() => navigation.navigate("StackScreen")}
                 style={{
                     backgroundColor: "purple",
                     padding: 10,
-                    marginTop: "20%",
+                    marginTop: "10%",
+                    marginBottom: "10%",
                     width: "50%",
                     alignSelf: "center",
                     borderRadius: 10,
@@ -61,6 +79,15 @@ const HomeScreen = () => {
                     Go to Stack
                 </Text>
             </TouchableOpacity>
+            {allowances.items.map(item => (
+                <Text key={item.id}
+                    style={{
+                        marginLeft:"10%",
+                        marginRight:"10%",
+                        marginBottom: "5%",
+                    }}
+                >* {item.title}</Text>
+            ))}
         </View>
     )
 }
